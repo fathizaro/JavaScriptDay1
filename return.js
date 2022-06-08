8ce3419cfce01c3d52577e4f137a13c1eb1e466d
fathizaro <63026012+fathizaro@users.noreply.github.com>
2022-06-07 20:53:18 -0400

    transfer- assignment

---
+++ return.js
@@ -0,0 +1,10 @@
+function handleSubmit (){
+
+const name = document.getElementById('name').value;
+const surname = document.getElementById('surname').value;
+
+localStorage.setItem("NAME", name)
+localStorage.setItem("SURNAME", surname)
+
+return;
+}
